83eed3b5599c4191d50b8381908cd59602840a20
Nico <nicolapicchio@gmail.com>
2021-12-15 12:25:33 +0000

    Exercise completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,16 @@
 // TODO: Write your function in this file
-
+function fizzBuzz(lowerNum, upperNum) {
+    const fizzBuzzArray = [];
+    for (let i = lowerNum; i <= upperNum; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArray.push('FizzBuzz')
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push('Fizz')
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push('Buzz')
+        } else fizzBuzzArray.push(i)
+    }
+    return fizzBuzzArray
+}
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
